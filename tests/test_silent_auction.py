6ac59ea2dfc4252595dc0719300d17afc33a4052
Aksel Yusein <akselyusein@Aksels-MacBook-Pro.local>
2025-06-29 00:30:48 +0100

    SPOR-31

--- tests/test_silent_auction.py
+++ tests/test_silent_auction.py
@@ -17,3 +17,21 @@ def test_find_winner_fail():
     expected = 'Aksel'
     result = find_winner(bidders=bidders)
     assert(expected!=result)
+
+def test_tie_breaker_success():
+    bidders = [{"name": 'Aksel', "bid": 350},
+               {"name": 'Izet', "bid": 350},
+               {"name": 'Harley', "bid": 40}
+    ]
+    expected = 'Aksel'
+    result = find_winner(bidders=bidders)
+    assert(expected==result)
+
+def test_tie_breaker_fail():
+    bidders = [{"name": 'Aksel', "bid": 350},
+               {"name": 'Izet', "bid": 350},
+               {"name": 'Harley', "bid": 40}
+    ]
+    expected = 'Aksel'
+    result = find_winner(bidders=bidders)
+    assert(expected!=result)
