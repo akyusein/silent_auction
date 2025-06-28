2444f54041d6562ab1090f5144b2b34a01a36222
Aksel Yusein <akselyusein@Aksels-MacBook-Pro.local>
2025-06-29 00:18:08 +0100

    SPOR-30

--- silent_auction.py
+++ silent_auction.py
@@ -3,11 +3,21 @@ import getpass
 import random
 from items_for_auction import options
 
+
 print(logo)
-print("Welome to this silent auction!")
+print("Welcome to this silent auction!")
 pick = str(random.choice(options))
-print(f"You're fighting to win {pick}! Good luck!")
-print("This is a test")
+print(f"You're fighting to win {pick}! Good luck!\n")
+
+def find_winner(bidders):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidders:
+        if bidder["bid"] > highest_bid:
+            highest_bid = bidder["bid"]
+            winner = bidder["name"]
+    print(f"\n🏆 {pick} goes to {winner} with a bid of £{highest_bid}!")
+    return winner
 
 def main():
     bidders = []
@@ -17,22 +27,20 @@ def main():
         name = input("What is your name? : ")
         bid = int(getpass.getpass(prompt='What is your bid?: £ ', stream=None))
         bidders.append({"name": name, "bid": bid})
-        more_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
 
-        if more_bidders == 'no':
-            bidding_open = False
-            find_winner(bidders)
+        while True:
+            more_bidders = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+            if more_bidders == 'no':
+                bidding_open = False
+                break
 
-def find_winner(bidders):
-    highest_bid = 0
-    winner = ""
-    for bidder in bidders:
-        if bidder["bid"] > highest_bid:
-            highest_bid = bidder["bid"]
-            winner = bidder["name"]
-        
-    print(f"{pick} goes to {winner} with a bid of £{highest_bid}")
-    return winner
-    
+            elif more_bidders == 'yes':
+                break
+            
+            else:
+                print("❌ Invalid input! Please type 'yes' or 'no'.\n")
+
+    find_winner(bidders)
+
 if __name__ == "__main__":
     main()
